d1da90f9c1eaaf41ba582e0d4472684d35ef69f0
Pavrenchy ULYAH <sgaitum@riseup.net>
2022-11-29 10:56:22 -0400

    Evolution fastify

--- index.js
+++ index.js
@@ -1,113 +1,86 @@
-
-// const { clientId, guildId, token, publicKey } = require('./config.json');
-require('dotenv').config()
-const APPLICATION_ID = process.env.APPLICATION_ID 
-const TOKEN = process.env.TOKEN 
-const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
-const GUILD_ID = process.env.GUILD_ID 
+const fastify = require("fastify");
+const{ InteractionResponseType, InteractionType, verifyKey } = require("discord-interactions");
+const rawBody = require("fastify-raw-body");
 
 
-const axios = require('axios')
-const express = require('express');
-const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
 
 
-const app = express();
-// app.use(bodyParser.json());
 
-const discord_api = axios.create({
-  baseURL: 'https://discord.com/api/',
-  timeout: 3000,
-  headers: {
-	"Access-Control-Allow-Origin": "*",
-	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-	"Access-Control-Allow-Headers": "Authorization",
-	"Authorization": `Bot ${TOKEN}`
-  }
+const server = fastify({
+    logger: true,
 });
 
 
 
 
-app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
-  const interaction = req.body;
 
-  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-    console.log(interaction.data.name)
-    if(interaction.data.name == 'yo'){
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: `Yo ${interaction.member.user.username}!`,
-        },
-      });
-    }
 
-    if(interaction.data.name == 'dm'){
-      // https://discord.com/developers/docs/resources/user#create-dm
-      let c = (await discord_api.post(`/users/@me/channels`,{
-        recipient_id: interaction.member.user.id
-      })).data
-      try{
-        // https://discord.com/developers/docs/resources/channel#create-message
-        let res = await discord_api.post(`/channels/${c.id}/messages`,{
-          content:'Yo! I got your slash command. I am not able to respond to DMs just slash commands.',
-        })
-        console.log(res.data)
-      }catch(e){
-        console.log(e)
+
+server.register(rawBody, {
+    runFirst: true,
+});
+
+
+
+
+
+server.get("/", (request, response) => {
+    server.log.info("Handling GET request");
+});
+
+
+
+
+server.addHook('preHandler', async (request, response) => {
+    // We don't want to check GET requests to our root url
+    if (request.method === 'POST') {
+      const signature = request.headers['x-signature-ed25519'];
+      const timestamp = request.headers['x-signature-timestamp'];
+      const isValidRequest = verifyKey(
+        request.rawBody,
+        signature,
+        timestamp,
+        process.env.PUBLIC_KEY
+      );
+      if (!isValidRequest) {
+        server.log.info('Invalid Request');
+        return response.status(401).send({ error: 'Bad request signature ' });
       }
-
-      return res.send({
-        // https://discord.com/developers/docs/interactions/receiving-and-responding#responding-to-an-interaction
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data:{
-          content:'👍'
-        }
-      });
     }
-  }
-
 });
 
 
 
-app.get('/register_commands', async (req,res) =>{
-  let slash_commands = [
-    {
-      "name": "yo",
-      "description": "replies with Yo!",
-      "options": []
-    },
-    {
-      "name": "dm",
-      "description": "sends user a DM",
-      "options": []
+
+
+
+
+
+
+server.post("/", async (request, response) => {
+    const message = request.body;
+    if (message.type === InteractionType.PING) {
+      server.log.info("Handling Ping request");
+      response.send({
+        type: InteractionResponseType.PONG,
+      });
+    } else {
+      server.log.error("Unknown Type");
+      response.status(400).send({ error: "Unknown Type" });
     }
-  ]
-  try
-  {
-    // api docs - https://discord.com/developers/docs/interactions/application-commands#create-global-application-command
-    let discord_response = await discord_api.put(
-      `/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`,
-      slash_commands
-    )
-    console.log(discord_response.data)
-    return res.send('commands have been registered')
-  }catch(e){
-    console.error(e.code)
-    console.error(e.response?.data)
-    return res.send(`${e.code} error from discord`)
-  }
-})
+});
 
 
-app.get('/', async (req,res) =>{
-  return res.send('Follow documentation ')
-})
 
 
-app.listen(8999, () => {
 
-})
 
+
+
+server.listen(3000, async (error, address) => {
+    if (error) {
+      server.log.error(error);
+      process.exit(1);
+    }
+    server.log.info(`server listening on ${address}`);
+});
