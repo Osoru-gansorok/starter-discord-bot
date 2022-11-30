03a0263734f136d66e2e80b19f51297dddd63d23
Pavrenchy ULYAH <sgaitum@riseup.net>
2022-11-30 03:33:51 -0400

    j

--- server.js
+++ server.js
@@ -22,9 +22,8 @@ const server = fastify({
 // });
 
 server.register(rawBody, {
-  runFirst: true,
   field: 'rawBody', // change the default request.rawBody property name
-  global: false, // add the rawBody to every request. **Default true**
+  global: true, // add the rawBody to every request. **Default true**
   encoding: 'utf8', // set it to false to set rawBody as a Buffer **Default utf8**
   runFirst: true, // get the body before any preParsing hook change/uncompress it. **Default false**
   routes: [] // array of routes, **`global`** will be ignored, wildcard routes not supported
